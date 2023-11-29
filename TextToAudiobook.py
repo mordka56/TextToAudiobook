7139edd5f2916d235916bf7f19a0ef932f3a2ba7
Jarosław Bogdański <mordka4428@gmail.com>
2023-11-29 14:53:04 +0100

    fixed file name

--- TextToAudiobook.py
+++ TextToAudiobook.py
@@ -1,16 +1,19 @@
-import pyttsx3
+from gtts import gTTS
+import os
 
 def read_book(file_path):
-    engine = pyttsx3.init()
     try:
         with open(file_path, 'r') as book:
             book_text = book.read()
-            engine.say(book_text)
-            engine.runAndWait()
+            tts = gTTS(text=book_text, lang='pl') # Można zmienić język, jeśli potrzebujesz
+            tts.save("book.mp3") # Zapisuje wypowiedziane słowa do pliku mp3
     except FileNotFoundError:
         print(f"Nie znaleziono pliku: {file_path}")
     except Exception as e:
         print(f"Wystąpił błąd: {e}")
+
+    # Odtwarzanie pliku mp3
+    os.system("start book.mp3") # 'start' dla Windows, 'open' dla macOS lub 'xdg-open' dla Linux
 
 # Użycie funkcji
 read_book("book.txt")
